f7125b22084eced88590bc2309d37b10d656469d
wimaks <squ1rtle.mg@gmail.com>
2022-11-11 00:26:36 +0600

    Закончил верстку попапа

--- js/app.js
+++ js/app.js
@@ -63,4 +63,70 @@ $(document).ready(function () {
             }
         ]
     });
+
+
+    // POPUP
+
+    $('.popup__list-link').on('click', function (event) {
+        event.preventDefault();
+
+        $(this).toggleClass('active');
+        $('.visit-master').text($(this).html())
+        $(event.currentTarget).next().toggleClass('active')
+    })
+
+    $('.popup-link').on('click', function (event) {
+        event.preventDefault();
+
+        $('.popup').toggleClass('active')
+    })
+
+    $('.popup__close, .popups__close').on('click', function (event) {
+        event.preventDefault();
+
+
+        $('.popup, .popup__list-card').removeClass('active')
+    })
+
+    $('.popup__close-second').on('click', function (event) {
+        event.preventDefault();
+
+        $('.popup__second').removeClass('active')
+    })
+
+    $('.popup__button').on('click', function (event) {
+        event.preventDefault();
+
+        let visitDate = $('#date').val(),
+            visitPhone = $('#phone').val(),
+            clientName = $('#name').val()
+
+        $('.visit-date').text(visitDate)
+        $('.visit-phone').text(visitPhone)
+        $('.client-name').text(clientName)
+        $('.popup__second').toggleClass('active')
+    })
+
+    $('.popup__times-link').on('click', function (event) {
+        event.preventDefault()
+
+        $(this).toggleClass('active')
+        let visitTime = $(this).html()
+        $('.visit-time').text(visitTime)
+    })
+
+    // SERVICES
+
+    $('.services__table-link').on('click', (event) => {
+        event.preventDefault()
+
+        $(event.currentTarget).closest('.services__table-block.hidden').toggleClass('visible')
+    })
+
+    $("#phone").mask("+7 (999) 999-9999");
+
+    $(".flatpickr").flatpickr();
+
+
+
 })
